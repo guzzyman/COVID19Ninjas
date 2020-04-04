80d1332e5e8bb78689394b37060675ecf54a37c0
Olotu Oluwagbemiga <gbenga.olotu@live.com>
2020-04-04 23:51:17 +0100

    commit - removed unwanted pages.

---
+++ COVID19Ninjas/Pages/Register.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace COVID19Ninjas.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public RegisterModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
